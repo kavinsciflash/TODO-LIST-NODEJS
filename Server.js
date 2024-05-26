be0f87c4af4b6c46802ab5b705f924c49efa1399
kavinprasanth <kavinprasanth7274@gmail.com>
2024-05-26 13:52:07 +0530

    node app updated

--- Server.js
+++ Server.js
@@ -15,7 +15,15 @@ const app = express()
 const PORT = process.env.PORT | 5000
 
 app.use(express.json())
-app.use(cors())
+
+const corsOptions = {
+    origin: 'http://34.229.14.151', // Allow requests from this origin
+    optionsSuccessStatus: 200 // Some legacy browsers choke on 204
+  };
+  
+  // Use CORS middleware with the specified options
+  app.use(cors(corsOptions));
+  
 
 mongoose.connect(MONGO_URI)
     .then(() => console.log("mongodb connected..."))
